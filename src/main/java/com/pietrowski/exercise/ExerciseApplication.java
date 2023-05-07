163f3d5082fce46a782a609a480d78df562fbb3a
Derigaaz <stachupietrowski@gmail.com>
2023-05-07 22:59:45 +0200

    added simple program logic to read the file from path and write the processed Objects in console

--- src/main/java/com/pietrowski/exercise/ExerciseApplication.java
+++ src/main/java/com/pietrowski/exercise/ExerciseApplication.java
@@ -1,26 +1,52 @@
 package com.pietrowski.exercise;
 
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootApplication
-public class ExerciseApplication {
+import static java.lang.System.exit;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ExerciseApplication.class, args);
-        FileInputStream inputStream = ExcelProcessingService.openInputStream("C:\\Users\\stach\\Desktop\\exercise\\annex_vi_clp_table_atp18_en.xlsx");
-        Optional<Workbook> workBook = Optional.ofNullable(ExcelProcessingService.getWorkBookFromStream(inputStream));
-        List<Substance> substances = workBook.map((InputProcessingService::createListOfSubstancesFromWorkbook)).orElseGet(ArrayList::new);
-        substances.stream().map(Substance::toString).forEachOrdered(System.out::println);
-        assert inputStream != null;
-        ExcelProcessingService.closeInputStream(inputStream);
+@SpringBootApplication
+public class ExerciseApplication implements CommandLineRunner {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication app = new SpringApplication(ExerciseApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
-
+    @Override
+    public void run(String... args) throws IOException {
+        while (true) {
+            System.out.println("Type 'quit' to exit the application or 'input' to input a new Excel file to process.");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+            String operation;
+            operation = reader.readLine();
+            switch (operation) {
+                case "quit" -> exit(0);
+                case "input" -> {
+                    System.out.println("Please enter path to the file you wish to process.");
+                    String filePath = reader.readLine();
+                    FileInputStream inputStream = ExcelProcessingService.openInputStream(filePath);
+                    Optional<Workbook> workBook = Optional.ofNullable(ExcelProcessingService.getWorkBookFromStream(inputStream));
+                    List<Substance> substances = workBook.map((InputProcessingService::createListOfSubstancesFromWorkbook)).orElseGet(ArrayList::new);
+                    substances.stream().map(Substance::toString).forEachOrdered(System.out::println);
+                    assert inputStream != null;
+                    ExcelProcessingService.closeInputStream(inputStream);
+                }
+                default -> System.out.println("Unrecognized operation.");
+            }
+        }
+    }
 }
