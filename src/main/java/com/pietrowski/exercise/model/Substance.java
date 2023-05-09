38cf172ad51b48c61d752802b2a36c60f3cbb90b
Derigaaz <stachupietrowski@gmail.com>
2023-05-09 19:44:13 +0200

    added h2 database to the project

--- src/main/java/com/pietrowski/exercise/model/Substance.java
+++ src/main/java/com/pietrowski/exercise/model/Substance.java
@@ -1,21 +1,26 @@
-package com.pietrowski.exercise;
+package com.pietrowski.exercise.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 @ToString
-public class Substance {
+public class Substance implements Serializable {
+    @Id
     private String indexNo;
     private String intChemId;
     private String ecNo;
