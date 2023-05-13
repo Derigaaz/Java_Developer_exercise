529f0b5c5a6e0685bffa64c9ba654c38db50c747
Derigaaz <stachupietrowski@gmail.com>
2023-05-14 01:42:09 +0200

    added no args constructor

--- src/main/java/com/pietrowski/exercise/model/Substance.java
+++ src/main/java/com/pietrowski/exercise/model/Substance.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @Entity
 @Table
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @ToString
 public class Substance implements Serializable {
